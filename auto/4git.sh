73f9ab8116f37954411081022d40e34652e26127
Hong Wu <lem-org@outlook.com>
2024-01-25 22:57:20 +0800

    build: add the preproccess.sh to hooks

--- auto/4git.sh
+++ auto/4git.sh
@@ -1,6 +1,11 @@
 #! /bin/bash -ex
 
+cd $(dirname $0)/..
+
 git config user.email lem-org@outlook.com
 git config core.quotepath false
 
 git submodule update --remote --merge
+
+git config core.hooksPath hooks
+cat ./.git/config
